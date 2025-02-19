e33f91e602b899d5f6af27f29a769359a060b10e
w4t3rcs <w4t3rofficial@gmail.com>
2025-02-20 00:26:28 +0100

    Some more methods

--- src/main/java/com/temporal/api/core/registry/factory/common/MenuTypeFactory.java
+++ src/main/java/com/temporal/api/core/registry/factory/common/MenuTypeFactory.java
@@ -4,14 +4,27 @@ import com.temporal.api.core.engine.io.IOHelper;
 import com.temporal.api.core.engine.io.context.InjectionContext;
 import net.minecraft.core.Holder;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.world.flag.FeatureFlagSet;
+import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.MenuType;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.common.extensions.IMenuTypeExtension;
+import net.neoforged.neoforge.network.IContainerFactory;
+import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 import java.util.function.Supplier;
 
 public class MenuTypeFactory implements ObjectFactory<MenuType<?>> {
     public static final DeferredRegister<MenuType<?>> MENU_TYPES = IOHelper.createRegistry(Registries.MENU);
+
+    public <T extends AbstractContainerMenu> DeferredHolder<MenuType<?>, MenuType<T>> create(String name, IContainerFactory<T> containerFactory) {
+        return MENU_TYPES.register(name, () -> IMenuTypeExtension.create(containerFactory));
+    }
+
+    public <T extends AbstractContainerMenu> DeferredHolder<MenuType<?>, MenuType<T>> create(String name, MenuType.MenuSupplier<T> container, FeatureFlagSet featureFlagSet) {
+        return MENU_TYPES.register(name, () -> new MenuType<>(container, featureFlagSet));
+    }
 
     @Override
     public Holder<MenuType<?>> create(String name, Supplier<MenuType<?>> menuTypeSupplier) {
