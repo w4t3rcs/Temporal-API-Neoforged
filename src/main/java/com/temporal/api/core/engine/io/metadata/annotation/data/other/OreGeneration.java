0224853ab993d1cec7993e5c70f12fa5a1f6b1fb
w4t3rcs <w4t3rofficial@gmail.com>
2025-02-20 18:51:05 +0100

    Some small refactorings

--- src/main/java/com/temporal/api/core/engine/io/metadata/annotation/data/other/OreGeneration.java
+++ src/main/java/com/temporal/api/core/engine/io/metadata/annotation/data/other/OreGeneration.java
@@ -11,25 +11,13 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface OreGeneration {
-    Configuration configuration();
-    Placement placement();
-    BiomeModifier biomeModifier();
-
-    @interface Configuration {
-        String[] replaceableBlocks();
-        int size() default 1;
-    }
-
-    @interface Placement {
-        OreRarity rarity() default OreRarity.COMMON;
-        int count() default 1;
-        OrePlacementShape shape() default OrePlacementShape.UNIFORM;
-        int from();
-        int to();
-    }
-
-    @interface BiomeModifier {
-        String biomeTag() default "is_overworld";
-        Class<?> biomeTagContainer() default Object.class;
-    }
+    String[] replaceableBlocks();
+    int size() default 1;
+    OreRarity rarity() default OreRarity.COMMON;
+    int count() default 1;
+    OrePlacementShape shape() default OrePlacementShape.UNIFORM;
+    int from();
+    int to();
+    String biomeTag() default "is_overworld";
+    Class<?> biomeTagContainer() default Object.class;
 }
--- src/main/java/com/temporal/api/core/engine/io/metadata/strategy/field/data/other/OreGenerationStrategy.java
+++ src/main/java/com/temporal/api/core/engine/io/metadata/strategy/field/data/other/OreGenerationStrategy.java
@@ -17,14 +17,11 @@ public class OreGenerationStrategy implements FieldAnnotationStrategy {
             field.setAccessible(true);
             DeferredBlock<?> registryObject = (DeferredBlock<?>) field.get(object);
             OreGeneration oreGeneration = field.getDeclaredAnnotation(OreGeneration.class);
-            OreGeneration.BiomeModifier annotationBiomeModifier = oreGeneration.biomeModifier();
-            Class<?> tagContainer = annotationBiomeModifier.biomeTagContainer();
+            Class<?> tagContainer = oreGeneration.biomeTagContainer();
             if (!tagContainer.equals(Object.class)) BiomeTagDynamicPreparer.TAG_CONTAINERS.add(tagContainer);
-            OreGeneration.Configuration annotationConfiguration = oreGeneration.configuration();
-            Ore.Configuration configuration = new Ore.Configuration(annotationConfiguration.replaceableBlocks(), annotationConfiguration.size());
-            OreGeneration.Placement annotationPlacement = oreGeneration.placement();
-            Ore.Placement placement = new Ore.Placement(annotationPlacement.rarity(), annotationPlacement.count(), annotationPlacement.shape(), annotationPlacement.from(), annotationPlacement.to());
-            Ore.BiomeModifier biomeModifier = new Ore.BiomeModifier(annotationBiomeModifier.biomeTag());
+            Ore.Configuration configuration = new Ore.Configuration(oreGeneration.replaceableBlocks(), oreGeneration.size());
+            Ore.Placement placement = new Ore.Placement(oreGeneration.rarity(), oreGeneration.count(), oreGeneration.shape(), oreGeneration.from(), oreGeneration.to());
+            Ore.BiomeModifier biomeModifier = new Ore.BiomeModifier(oreGeneration.biomeTag());
             Ore ore = new Ore(IOHelper.getResourceId(registryObject.getKey()), configuration, placement, biomeModifier);
             GenerationFeaturesDescriptionContainer.ORES.put(registryObject, ore);
         }
