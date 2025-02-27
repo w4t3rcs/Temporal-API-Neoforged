e5b968a90dc9ffef7d38eb83d8b8a9099911b351
w4t3rcs <w4t3rofficial@gmail.com>
2025-02-28 00:30:21 +0100

    Some fixes

--- src/main/java/com/temporal/api/core/event/data/model/item/BowModelProviderStrategy.java
+++ src/main/java/com/temporal/api/core/event/data/model/item/BowModelProviderStrategy.java
@@ -1,7 +1,6 @@
 package com.temporal.api.core.event.data.model.item;
 
 import net.minecraft.client.data.models.ItemModelGenerators;
-import net.minecraft.client.data.models.model.ModelTemplates;
 import net.minecraft.world.item.Item;
 import net.neoforged.neoforge.registries.DeferredItem;
 
@@ -13,7 +12,6 @@ public class BowModelProviderStrategy extends AbstractItemModelProviderStrategy<
     @Override
     public void registerItemModel(DeferredItem<Item> itemRegistry, Object... additionalData) {
         Item item = itemRegistry.get();
-        this.getItemModels().generateFlatItem(item, ModelTemplates.FLAT_ITEM);
         this.getItemModels().generateBow(item);
     }
 }
