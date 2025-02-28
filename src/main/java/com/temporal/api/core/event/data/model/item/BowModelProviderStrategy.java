5db413256ad0482479271d351748fd544d46de4a
w4t3rcs <w4t3rofficial@gmail.com>
2025-02-28 09:42:48 +0100

    ModelFixes

--- src/main/java/com/temporal/api/core/event/data/model/item/BowModelProviderStrategy.java
+++ src/main/java/com/temporal/api/core/event/data/model/item/BowModelProviderStrategy.java
@@ -1,6 +1,10 @@
 package com.temporal.api.core.event.data.model.item;
 
 import net.minecraft.client.data.models.ItemModelGenerators;
+import net.minecraft.client.data.models.model.ItemModelUtils;
+import net.minecraft.client.data.models.model.ModelTemplates;
+import net.minecraft.client.renderer.item.ItemModel;
+import net.minecraft.client.renderer.item.properties.numeric.UseDuration;
 import net.minecraft.world.item.Item;
 import net.neoforged.neoforge.registries.DeferredItem;
 
@@ -12,6 +16,15 @@ public class BowModelProviderStrategy extends AbstractItemModelProviderStrategy<
     @Override
     public void registerItemModel(DeferredItem<Item> itemRegistry, Object... additionalData) {
         Item item = itemRegistry.get();
-        this.getItemModels().generateBow(item);
+        ItemModelGenerators itemModels = this.getItemModels();
+        ItemModel.Unbaked itemModel = ItemModelUtils.plainModel(itemModels.createFlatItemModel(item, ModelTemplates.BOW));
+        ItemModel.Unbaked pullingBow0 = ItemModelUtils.plainModel(itemModels.createFlatItemModel(item, "_pulling_0", ModelTemplates.BOW));
+        ItemModel.Unbaked pullingBow1 = ItemModelUtils.plainModel(itemModels.createFlatItemModel(item, "_pulling_1", ModelTemplates.BOW));
+        ItemModel.Unbaked pullingBow2 = ItemModelUtils.plainModel(itemModels.createFlatItemModel(item, "_pulling_2", ModelTemplates.BOW));
+        itemModels.itemModelOutput.accept(item, ItemModelUtils.conditional(ItemModelUtils.isUsingItem(), ItemModelUtils.rangeSelect(
+                new UseDuration(false), 0.05F, pullingBow0,
+                ItemModelUtils.override(pullingBow1, 0.65F),
+                ItemModelUtils.override(pullingBow2, 0.9F)), itemModel)
+        );
     }
 }
--- src/main/java/com/temporal/api/core/event/data/model/item/CrossbowModelProviderStrategy.java
+++ src/main/java/com/temporal/api/core/event/data/model/item/CrossbowModelProviderStrategy.java
@@ -1,6 +1,12 @@
 package com.temporal.api.core.event.data.model.item;
 
 import net.minecraft.client.data.models.ItemModelGenerators;
+import net.minecraft.client.data.models.model.ItemModelUtils;
+import net.minecraft.client.data.models.model.ModelTemplates;
+import net.minecraft.client.renderer.item.ItemModel;
+import net.minecraft.client.renderer.item.properties.numeric.CrossbowPull;
+import net.minecraft.client.renderer.item.properties.select.Charge;
+import net.minecraft.world.item.CrossbowItem;
 import net.minecraft.world.item.Item;
 import net.neoforged.neoforge.registries.DeferredItem;
 
@@ -12,6 +18,21 @@ public class CrossbowModelProviderStrategy extends AbstractItemModelProviderStra
     @Override
     public void registerItemModel(DeferredItem<Item> itemRegistry, Object... additionalData) {
         Item item = itemRegistry.get();
-        this.getItemModels().generateCrossbow(item);
+        ItemModelGenerators itemModels = this.getItemModels();
+        ItemModel.Unbaked crossbowModel = ItemModelUtils.plainModel(itemModels.createFlatItemModel(item, ModelTemplates.CROSSBOW));
+        ItemModel.Unbaked crossbowPulling0 = ItemModelUtils.plainModel(itemModels.createFlatItemModel(item, "_pulling_0", ModelTemplates.CROSSBOW));
+        ItemModel.Unbaked crossbowPulling1 = ItemModelUtils.plainModel(itemModels.createFlatItemModel(item, "_pulling_1", ModelTemplates.CROSSBOW));
+        ItemModel.Unbaked crossbowPulling2 = ItemModelUtils.plainModel(itemModels.createFlatItemModel(item, "_pulling_2", ModelTemplates.CROSSBOW));
+        ItemModel.Unbaked crossbowArrow = ItemModelUtils.plainModel(itemModels.createFlatItemModel(item, "_arrow", ModelTemplates.CROSSBOW));
+        ItemModel.Unbaked crossbowFirework = ItemModelUtils.plainModel(itemModels.createFlatItemModel(item, "_firework", ModelTemplates.CROSSBOW));
+        itemModels.itemModelOutput.accept(item, ItemModelUtils.conditional(ItemModelUtils.isUsingItem(), ItemModelUtils.rangeSelect(
+                new CrossbowPull(), crossbowPulling0,
+                ItemModelUtils.override(crossbowPulling1, 0.58F),
+                ItemModelUtils.override(crossbowPulling2, 1.0F)),
+                ItemModelUtils.select(new Charge(), crossbowModel,
+                        ItemModelUtils.when(CrossbowItem.ChargeType.ARROW, crossbowArrow),
+                        ItemModelUtils.when(CrossbowItem.ChargeType.ROCKET, crossbowFirework))
+                )
+        );
     }
 }
