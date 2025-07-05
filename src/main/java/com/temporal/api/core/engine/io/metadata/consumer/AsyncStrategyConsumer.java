a3fe9a72c0c2c789c4aa8183092966195dca9833
w4t3rcs <w4t3rofficial@gmail.com>
2025-07-05 13:04:23 +0200

    Migrated to Executors.newWorkStealingPool()

--- src/main/java/com/temporal/api/core/engine/io/metadata/consumer/AsyncStrategyConsumer.java
+++ src/main/java/com/temporal/api/core/engine/io/metadata/consumer/AsyncStrategyConsumer.java
@@ -7,21 +7,26 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class AsyncStrategyConsumer implements AnnotationStrategyConsumer {
     @Override
     public <T extends ObjectStrategy<?>> void execute(AnnotationExecutor<T> executor, List<T> strategies, Set<Class<?>> source) {
-        ForkJoinPool pool = ForkJoinPool.commonPool();
-        List<CompletableFuture<Void>> futures = new ArrayList<>();
-        strategies.forEach(strategy -> {
-            source.forEach(clazz -> {
-                futures.add(CompletableFuture.runAsync(() -> {
-                    executor.tryExecute(strategy, clazz);
-                }, pool));
-            });
-        });
+        ExecutorService pool = Executors.newWorkStealingPool();
+        try {
+            List<CompletableFuture<Void>> futures = new ArrayList<>();
+            for (T strategy : strategies) {
+                for (Class<?> clazz : source) {
+                    futures.add(CompletableFuture.runAsync(() -> {
+                        executor.tryExecute(strategy, clazz);
+                    }, pool));
+                }
+            }
 
-        CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
+            CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
+        } finally {
+            pool.shutdown();
+        }
     }
 }
